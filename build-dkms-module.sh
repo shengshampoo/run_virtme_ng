5c9cad011778bba7d5763fb89241024318e72df6
shengshampoo <shengzh@tutanota.com>
2026-02-02 12:18:28 +0800

    Update build-dkms-module.sh

--- build-dkms-module.sh
+++ build-dkms-module.sh
@@ -6,12 +6,13 @@ mkdir -p /work/artifact
 
 # linux xanmod kernel
 
+apt update -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true
 apt install --yes jq dkms lzip
 wget -qO - https://gist.githubusercontent.com/shengshampoo/62d3a4f2f187927db933673cc715f8ef/raw/cec7264a320fad6027f78edc6296d75592d6e2a1/archive.key | gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
 KRV=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=6.18" | jq -r '.[0].name'  | sed -e s/-.*//)-x64v3-xanmod1
 KRW=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=rt" | jq -r '.[0].name'  | sed -e s/-x.*//)-x64v3-xanmod1
-apt update
+apt update -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true
 apt install linux-image-$KRV linux-headers-$KRV linux-image-$KRW linux-headers-$KRW
 
 # bcachefs dkms module

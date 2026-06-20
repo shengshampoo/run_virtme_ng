698c598c1ca2f3e862809d5d5b72dc4d512cb2c0
shengshampoo <shengzh@tutanota.com>
2026-06-20 11:10:56 +0000

    Update build-dkms-module.sh

--- build-dkms-module.sh
+++ build-dkms-module.sh
@@ -13,9 +13,10 @@ echo 'deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xa
 KRV=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=6.18" | jq -r '.[0].name'  | sed -e s/-.*//)-x64v3-xanmod1
 KRW=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=rt" | jq -r '.[0].name'  | sed -e s/-x.*//)-x64v3-xanmod1
 KRX=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=7.0" | jq -r '.[0].name'  | sed -e s/-.*//)-x64v3-xanmod1
+KRY=$(wget -qO-  "https://gitlab.com/api/v4/projects/51590166/repository/tags?order_by=updated&search=7.1" | jq -r '.[0].name'  | sed -e s/-.*//)-x64v3-xanmod1
 apt update -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true
-apt install linux-image-$KRV linux-headers-$KRV linux-image-$KRW linux-headers-$KRW linux-image-$KRX linux-headers-$KRX
-
+apt install linux-image-$KRV linux-headers-$KRV linux-image-$KRW linux-headers-$KRW linux-image-$KRX linux-headers-$KRX linux-image-$KRY linux-headers-$KRY
+ 
 # bcachefs dkms module
 wget -qO- https://apt.bcachefs.org/apt.bcachefs.org.asc | tee /etc/apt/trusted.gpg.d/apt.bcachefs.org.asc
 wget -qO- "https://gist.githubusercontent.com/shengshampoo/1d0a95c771a9be46a8ffba981ccf959b/raw/9b4e33dd2e2401a38e6595be951da68e21f43b66/apt.bcachefs.org.sources" | tee /etc/apt/sources.list.d/apt.bcachefs.org.sources
@@ -29,3 +30,5 @@ cp /lib/modules/$KRW/updates/dkms/bcachefs.ko .
 tar -I 'lzip -9' --remove-files -vcf bcachefs.ko-$KRW.tlz ./bcachefs.ko
 cp /lib/modules/$KRX/updates/dkms/bcachefs.ko .
 tar -I 'lzip -9' --remove-files -vcf bcachefs.ko-$KRX.tlz ./bcachefs.ko
+cp /lib/modules/$KRY/updates/dkms/bcachefs.ko .
+tar -I 'lzip -9' --remove-files -vcf bcachefs.ko-$KRY.tlz ./bcachefs.ko
